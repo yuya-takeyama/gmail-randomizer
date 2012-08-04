a6b70fbc2d77ed8cf0209ee1dc9d0e8c2b88912d
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-08-05 02:28:19 +0900

    Twitter Bootstrap integration

--- js/script.js
+++ js/script.js
@@ -1,10 +1,42 @@
-/* Author:
+/* Author: Yuya Takeyama
 
 */
+jQuery(document).ready(function () {
+  var $ = jQuery;
 
+  var $account = $('#account')
+    , $mail_with_plus = $('#mail_with_plus')
+    , $mail_with_dots = $('#mail_with_dots');
 
+  $account.val(localStorage.getItem('account'));
 
+  var createArray = function (len) {
+    result = [];
+    for (var i = 0; i < len; i++) {
+      result.push(null);
+    }
+    return result;
+  };
 
+  var randStr = (function () {
+    var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split('');
+    return function (len) {
+      return (createArray(8)).reduce(function (str, chr) {
+        return str + chars[Math.floor(Math.random() * chars.length)];
+      }, '');
+    };
+  })();
 
+  $('#randomizer').on('submit', function (event) {
+    event.preventDefault();
 
+    localStorage.setItem('account', $account.val());
 
+    var account = $account.val().split('').reduce(function (str, chr) {
+      return str + (createArray(parseInt(Math.random() * 3)).join('.')) + chr;
+    });
+
+    $mail_with_plus.text($account.val() + '+' + randStr(8) + '@gmail.com');
+    $mail_with_dots.text(account + '@gmail.com');
+  });
+});
